b1715d0a55acadb961e4c2dae8d51841fe037474
Ahmet Salih Seçkin <36454018+seckinsalihDev@users.noreply.github.com>
2022-01-21 14:55:56 +0300

    first commit

---
+++ BlockchainASS/BlockchainASS/Blockchain.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockchainASSS
+{
+    public class Blockchain
+    {
+        public IList<Transaction> PendingTransactions = new List<Transaction>();
+        public IList<Block> Chain { get; set; }
+        public int difficulty { get; set; } = 2;
+        public int Reward { get; set; } = 1;
+        public Blockchain()
+        {
+            InitializeChain();
+            AddgenesisBlock();
+        }
+        public void InitializeChain()
+        {
+            Chain = new List<Block>();
+        }
+        public Block CreateGenesisBlock()
+        {
+            Block block = new Block(DateTime.Now, null, PendingTransactions);
+            block.Mine(difficulty);
+            PendingTransactions = new List<Transaction>();
+
+            return block;
+        }
+        //Doğuş bloğu
+        public void AddgenesisBlock()
+        {
+            Chain.Add(CreateGenesisBlock());
+        }
+        public Block GetLastestBlock()
+        {           
+            return Chain[Chain.Count - 1];
+        }
+        public void AddBlock(Block block)
+        {
+            Block lastestblock = GetLastestBlock();
+            block.Index=lastestblock.Index +1;
+            block.PreviousHash=lastestblock.Hash;
+            block.Hash=block.CalculateHash();
+            block.Mine(this.difficulty);
+            Chain.Add(block);
+        }
+        public void CreateTransaction(Transaction transaction)
+        {
+            PendingTransactions.Add(transaction);
+        }
+
+        public void ProcessPendingTransactions(string minerAdress)
+        {
+            CreateTransaction(new Transaction(null, minerAdress, Reward));
+            Block block = new Block(DateTime.Now, GetLastestBlock().Hash, PendingTransactions);
+            AddBlock(block);
+            PendingTransactions = new List<Transaction>();
+        }
+        public bool IsValid()
+        {
+            /* Şuanki blok ve bir önceki blokların hash değerlerini karşılaştırır 
+             * ve bloğun geçerliliğini kontrol eder */
+            for (int i=1; i<Chain.Count; i++)
+            {
+                Block currentBlock = Chain[i];
+                Block previousBlock = Chain[i-1];
+                if (currentBlock.Hash!=currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+                if (currentBlock.PreviousHash != previousBlock.Hash) 
+                { 
+                    return false; 
+                }
+            }
+            return true;
+        }
+        public int GetBalance(string adress)
+        {
+            int balance = 0;
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                for(int j = 0; j < Chain[i].Transactions.Count; j++)
+                {
+                    var transaction = Chain[i].Transactions[j];
+                    if (transaction.FromAdress == adress)
+                    {
+                        balance -= transaction.Amount;
+                    }
+                    if (transaction.ToAddress == adress)
+                    {
+                        balance += transaction.Amount;
+                    }
+                }
+            }
+            return balance;
+        }
+    }
+}
---
+++ BlockchainASS/BlockchainASS/block.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BlockchainASSS
+{
+    public class Block
+    {
+        /*   Blok yapımızın içinde gerekli değişkenler   */
+        public int Index { get; set; } 
+        public DateTime TimeStamp { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+
+        public IList<Transaction> Transactions;
+       
+      //public string Data { get; set; }
+
+        public int Nonce { get; set; } = 0;
+        public Block(DateTime timeStamp, string preivousHash, IList<Transaction> transactions)
+        {
+            Index = 0;
+            TimeStamp = timeStamp;
+            PreviousHash = preivousHash;
+            Transactions = transactions;
+
+        }
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+            /*  TimeStamp, PreviousHash, Data yı çekip byte çevirecek ve inbytes değişkenimizin içine atar, 
+             *  Ardından outbytes değişkenine inbytes değişkeninin sha256 uygulanır.
+             *  outbytes değişkeni string e çevirilip geri döndürülür.
+             */
+            byte[] inbytes = Encoding.ASCII.GetBytes($"{TimeStamp}-{PreviousHash ?? ""}-{JsonConvert.SerializeObject(Transactions)}--{Nonce}");
+            byte[] outbytes = sha256.ComputeHash(inbytes);
+            return Convert.ToBase64String(outbytes);
+        }
+        public void Mine(int difficulty)
+        {
+            var leadingZeros = new string('0', difficulty);
+            while (this.Hash==null || this.Hash.Substring(0, difficulty) != leadingZeros)
+            {
+                this.Nonce++;
+                this.Hash= this.CalculateHash();
+            }
+        }
+    }
+}
